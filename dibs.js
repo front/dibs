6507e26c00c3f59a705f0c498e3d525f5776f904
Élio Cró <elio@front.no>
2017-12-15 15:43:16 +0000

    Refactor to use promises throughout code

--- dibs.js
+++ dibs.js
@@ -1,165 +1,140 @@
 'use strict';
-
-/**
+/*
  * A simple wrapper for the [DIBS Payment Services](http://www.dibspayment.com/) API
  */
-var q = require('q');
-var request = require('request');
-var mac = require('./mac');
 
-request.defaults({
+require('request').defaults({
   jar: false,
   pool: false
 });
+const request = require('request-promise-native');
+const mac = require('./mac');
+
 
 module.exports = {
 
-  /**
+  /*
    * Default HMAC Key to encode the requests
   */
   hmacKey: '',
 
-  /**
+  /*
    * JSON Endpoint for card authorizations
   */
   authorizeCardUri: 'https://api.dibspayment.com/merchant/v1/JSON/Transaction/AuthorizeCard',
 
-  /**
+  /*
    * This service performs the first part of a credit card transaction (the authorisation).
    * The authorisation includes credit and debit card control and reservation of
    * the required amount for later capture.
   */
-  authorizeCard: function(options, hmacKey) {
+  authorizeCard (options, hmacKey) {
     return this.dibsRequest(options, hmacKey, this.authorizeCardUri);
   },
 
-  /**
+  /*
    * JSON Endpoint for ticket creation
   */
   createTicketUri: 'https://api.dibspayment.com/merchant/v1/JSON/Transaction/CreateTicket',
 
-  /**
+  /*
    * This service performs a credit and debit card check and saves the credit card
    * information for recurring payments.
   */
-  createTicket: function(options, hmacKey) {
+  createTicket (options, hmacKey) {
     return this.dibsRequest(options, hmacKey, this.createTicketUri);
   },
 
-  /**
+  /*
    * JSON Endpoint for ticket authorizations
   */
   authorizeTicketUri: 'https://api.dibspayment.com/merchant/v1/JSON/Transaction/AuthorizeTicket',
 
-  /**
+  /*
    * Make a recurring payment using a ticket previously created via the
    * createTicket service.
   */
-  authorizeTicket: function(options, hmacKey) {
+  authorizeTicket (options, hmacKey) {
     return this.dibsRequest(options, hmacKey, this.authorizeTicketUri);
   },
 
-  /**
+  /*
    * JSON Endpoint for capturing transactions
   */
   captureTransactionUri: 'https://api.dibspayment.com/merchant/v1/JSON/Transaction/CaptureTransaction',
 
-  /**
+  /*
    * The second part of any transaction is the capture process. Usually this take place
    * at the time of shipping the goods to the customer.
   */
-  captureTransaction: function(options, hmacKey) {
+  captureTransaction (options, hmacKey) {
     return this.dibsRequest(options, hmacKey, this.captureTransactionUri);
   },
 
-  /**
+  /*
    * JSON Endpoint for cancelling transactions
   */
   cancelTransactionUri: 'https://api.dibspayment.com/merchant/v1/JSON/Transaction/CancelTransaction',
 
-  /**
+  /*
    * The cancelTransaction service cancels an authorization. If the acquirer used
    * supports reversals, the system automatically sends one such along and thereby
    * releasing any reserved amounts.
   */
-  cancelTransaction: function(options, hmacKey) {
+  cancelTransaction (options, hmacKey) {
     return this.dibsRequest(options, hmacKey, this.cancelTransactionUri);
   },
 
-  /**
+  /*
    * JSON Endpoint for refunding transactions
   */
   refundTransactionUri: 'https://api.dibspayment.com/merchant/v1/JSON/Transaction/RefundTransaction',
 
-  /**
+  /*
    * The refundTransaction service refunds a captured transaction and transfers the money
    * back to the card holders account.
   */
-  refundTransaction: function(options, hmacKey) {
+  refundTransaction (options, hmacKey) {
     return this.dibsRequest(options, hmacKey, this.refundTransactionUri);
   },
 
-  /**
+  /*
    * Ping satellite endpoint
   */
   pingUri: 'https://api.dibspayment.com/merchant/v1/JSON/Transaction/Ping',
 
-  /**
+  /*
    * The ping service checks if satellite system is up and running.
   */
-  pingSatellite: function () {
-    var d = q.defer();
-
-    request({
+  pingSatellite () {
+    return request({
       uri: this.pingUri,
       json: true
-    },
-    function(err, res, body) {
-      if (err) {
-        return d.reject(err);
-      }
-      d.resolve(body);
     });
-
-    return d.promise;
   },
 
-  /**
+  /*
    * Executes the https request to the DIBS server and fulfills the promise
    *  with the response JSON Object
   */
-  dibsRequest: function(options, hmacKey, uri) {
-    if (this.testMode) {
+  dibsRequest (options, hmacKey, uri) {
+    if(this.testMode) {
       options.test = true;
     }
-    if (!hmacKey && !this.hmacKey) {
-      return q.reject(new Error('Merchant\'s hmacKey is required'));
+    if(!hmacKey && !this.hmacKey) {
+      throw new Error('Merchant\'s hmacKey is required');
     }
 
-    var d = q.defer();
-
-    //Calculate authentication code
+    // Calculate authentication code
     options.MAC = mac.calculateMAC(options, hmacKey || this.hmacKey);
+
     // Post data to DIBS
-    request.post({
+    return request.post({
       uri: uri,
       form: {
         request: JSON.stringify(options)
       },
       json: true
-    },
-    function(err, res, body) {
-      if (err) {
-        return d.reject(err);
-      }
-      try {
-        d.resolve(body);
-      }
-      catch (err) {
-        d.reject(err);
-      }
     });
-
-    return d.promise;
   }
 };
--- mac.js
+++ mac.js
@@ -1,40 +1,38 @@
 'use strict';
-
-/**
+/*
  * MAC calculation tool (for DIBS Payment Services)
-*/
-var crypto = require('crypto');
+ */
+
+const crypto = require('crypto');
+
 
 module.exports = {
-  /**
+  /*
    * Calculates the MAC key to authenticate the request
   */
-  calculateMAC: function(params, hmacKey) {
-    var query = '';
-    var keys = Object.keys(params).sort();
+  calculateMAC (params, hmacKey) {
+    let query = '';
+    const keys = Object.keys(params).sort();
 
     // Form the querystring
-    for (var i = 0; i < keys.length; i++) {
+    for(let i = 0; i < keys.length; i++) {
       query += '&' + keys[i] + '=' + encodeURIComponent(params[keys[i]]);
     }
     query = query.substr(1);
-    // console.log('X-QRY:', query);
 
     // Encode with the hmac key
-    var hmac = crypto.createHmac('sha256', hex2asc(hmacKey));
-    var hash = hmac.update(query).digest('hex');
-    // console.log('X-MAC:', hash);
-
+    const hmac = crypto.createHmac('sha256', hex2asc(hmacKey));
+    const hash = hmac.update(query).digest('hex');
     return hash;
   }
 };
 
-/**
+/*
  * Converts an HEX String to ASCII
 */
-function hex2asc(str) {
-  var res = '';
-  for (var b = 0; b < str.length; b += 2) {
+function hex2asc (str) {
+  let res = '';
+  for (let b = 0; b < str.length; b += 2) {
     res += String.fromCharCode(parseInt(str.substr(b, 2), 16));
   }
   return res;
